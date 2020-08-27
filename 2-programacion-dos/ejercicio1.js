505fedb66c0fd47fa9332b589920aea2dc72f775
Dago R <dagoramirezserna@gmail.com>
2020-08-26 22:20:36 -0500

    Se avanza hasta el ejercicio 3 de la unidad 2

---
+++ 2-programacion-dos/ejercicio1.js
@@ -0,0 +1,4 @@
+console.log("Ejercicio 1 unidad 2:")
+console.log('Hola');
+const age = 56;
+console.log(age + "\n\n");
---
+++ 2-programacion-dos/ejercicio3.js
@@ -0,0 +1,9 @@
+console.log("Ejercicio 3 unidad 2:")
+const kiwi = 2;
+const valorKiwi = 5;
+const pera = 3;
+const valorPera = 2;
+const uva = 0.5;
+const valorUva = 4;
+const total = (kiwi * valorKiwi) + (pera * valorPera) + (uva * valorUva);
+console.log("Valor total: ", total, "\n\n");
